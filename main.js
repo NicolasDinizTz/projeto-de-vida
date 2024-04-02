475d5aaf8bdb726c81d292102db01a0595010314
NicolasDinizTz <147161700+NicolasDinizTz@users.noreply.github.com>
2024-04-02 11:52:29 -0300

    nnn

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ console.log(botoes)
 for(let i=0; i <  botoes.length; i++){
       botoes[i].onclick = function(_){
 
+            for(let j=0;j<botoes.length;j++){
+                  botoes[j].classList.remove("ativo");
+       }
+      
+       botoes[i].classList.remove("ativo")
       }
 
 console.log(i)
