d0328425643d6fdd682dbe9f9b60db7179197a58
NicolasDinizTz <147161700+NicolasDinizTz@users.noreply.github.com>
2024-04-09 07:40:51 -0300

    jhjtytf

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes)
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0; i <  botoes.length; i++){
       botoes[i].onclick = function(_){
 
             for(let j=0;j<botoes.length;j++){
                   botoes[j].classList.remove("ativo");
+                  textos[j].classList.remove("ativo");
        }
-      
-       botoes[i].classList.remove("ativo")
-      }
 
-console.log(i)
+             botoes[i].classList.add("ativo");
+             textos[i].classList.add("ativo");
+      }    
 }
+
+const contadores = document.querySelectorAll(".contador");
+
+contadores[0].textContent = "Contagem regressiva";
